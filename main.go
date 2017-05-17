0c9578482c046374d9e84afd9f0054ead76735e2
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2017-05-17 01:22:56 -0300

    Working on websocket client

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
-import (
-	"log"
-	"time"
-
-	"github.com/gorilla/websocket"
-	"github.com/migueleliasweb/pingo-agent/ws"
-)
+import "github.com/migueleliasweb/pingo-agent/ws"
 
 func main() {
-	conn := ws.Connect("sockb.in", "/repeat/5")
+	domain := "sockb.in"
+	path := "/repeat/5"
+	tags := []string{}
+	tags[0] = "foo"
+	tags[1] = "bar"
 
-	go func() {
-		conn.WriteMessage(websocket.TextMessage, []byte("FOOOOO"))
-		time.Sleep(1500 * time.Millisecond)
-		conn.WriteMessage(websocket.TextMessage, []byte("BARRR"))
-	}()
-
-	for {
-		_, msg, err := conn.ReadMessage()
-		log.Println("Err:", err)
-		log.Println("Message:", string(msg))
-	}
+	wsClient := ws.New()
+	wsClient.Setup(
+		domain,
+		path,
+		tags,
+	)
 }
--- ws/client.go
+++ ws/client.go
@@ -3,19 +3,73 @@ package ws
 import (
 	"log"
 	"net/url"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-func Connect(host string, path string) *websocket.Conn {
-	u := url.URL{Scheme: "ws", Host: host, Path: path}
-	log.Printf("Connecting to %s", u.String())
+type wsClient struct {
+	conn *websocket.Conn
+	host string
+	path string
+	tags []string
+}
 
+type wsHandshake struct {
+	Ready bool
+}
+
+//New Returns a new instance of wsClient
+func New() *wsClient {
+	return &wsClient{}
+}
+
+//Setup Configures a wsClient instance
+func (ws *wsClient) Setup(host string, path string, tags []string) {
+	ws.host = host
+	ws.path = path
+	ws.tags = tags
+
+	ws.connect()
+	ws.sendHandshake()
+}
+
+func (ws *wsClient) connect() {
+	u := url.URL{Scheme: "ws", Host: ws.host, Path: ws.path}
+
+	start := time.Now()
+	log.Printf("Connecting to %s.", u.String())
 	WSConnection, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	log.Printf("Connected in %s.", time.Since(start)/time.Nanosecond)
 
 	if err != nil {
 		log.Fatalf("Could not connect to %s, reason: %s", u.String(), err)
 	}
 
-	return WSConnection
+	ws.conn = WSConnection
 }
+
+// func (ws *wsClient) closeConnHandler() func(code int, text string) error {
+// 	return ws.conn.handleClose
+// }
+
+func (ws *wsClient) writeJSON(data interface{}) {
+	ws.conn.WriteJSON(data)
+}
+
+func (ws *wsClient) readMessage() (int, []byte, error) {
+	return ws.conn.ReadMessage()
+}
+
+func (ws *wsClient) sendHandshake() {
+	log.Println("Sending initial message.")
+	ws.writeJSON(wsHandshake{Ready: true})
+	log.Println("Message sent.")
+}
+
+// func (ws *wsClient) run() {
+// 	log.Println("Starting to listening for messages from the server.")
+// 	for {
+// 		msgType, msgData, msgErr := ws.readMessage()
+// 	}
+// }
